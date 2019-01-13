2769eb93eff77262143ff72d720a66ad48eb0f78
Ian Wallace <ian.wallace2560@gmail.com>
2019-01-13 18:54:13 -0500

    style update

--- ProgramA.c
+++ ProgramA.c
@@ -1,10 +1,9 @@
 /*	
 	Gotoh's Algorithm
 	Implemented by Ian Wallace
-
 	---------------------------
 	Returns cost of optimal conversion using 
-	gotoh's algorithm (fig. 1(A))
+	gotoh's algorithm (fig. 1(A) in Miller-Myters (1988))
 */
 
 #include <float.h>
@@ -19,16 +18,16 @@
 #define MISMATCH 1  // Cost of a mismatch
 #define UNDEFINED -(DBL_MAX) 
 
-double gotohAlgorithm(char *seqA, char *seqB);
-double **init2DArray(double **array, int m, int n);
-void printArray(double **array, int m, int n);
+double cost_table(char a, char b, double costs[][128]);
+double gotoh_algorithm(char *sA, char *sB);
+double **init_2d_array(double **array, int M, int N);
 double min2(double a, double b);
 double min3(double a, double b, double c);
-double costTable(char a, char b, double costs[][128]);
+void print_2d_array(double **array, int M, int N);
 
 int main(int argc, char **argv)
 {
-	char *seqA, *seqB;
+	char *sA, *sB;
 
 	if(argc != 3)
 	{
@@ -36,59 +35,49 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
-	seqA = argv[1];
-	seqB = argv[2];
+	sA = argv[1];
+	sB = argv[2];
 
-	printf("Cost is %.2lf", gotohAlgorithm(seqA, seqB));
+	printf("Cost is %.2lf", gotoh_algorithm(sA, sB));
 
 	return 0;
 }
 
-void printArrays(double **arrayC, double **arrayD, double **arrayI,
-				 int M, int N)
+void print_2d_arrays(double **C, double **D, double **I, int M, int N)
 {
 	printf("Array C\n");
-	printArray(arrayC, M, N); printf("\n");
+	print_2d_array(C, M, N); printf("\n");
 	printf("Array D\n");
-	printArray(arrayD, M, N); printf("\n");
+	print_2d_array(D, M, N); printf("\n");
 	printf("Array I\n");
-	printArray(arrayI, M, N);
+	print_2d_array(I, M, N);
 }
 
 
-double gotohAlgorithm(char *seqA, char *seqB)
+double gotoh_algorithm(char *sA, char *sB)
 {
-	double **arrayC, **arrayD, **arrayI;
+	double **C, **D, **I;
 	double costs[128][128];
 	double t;
 	int M, N, i, j;
 
 
-	M = strlen(seqA) + 1;
-	N = strlen(seqB) + 1;
+	M = strlen(sA) + 1;
+	N = strlen(sB) + 1;
 
-	arrayC = init2DArray(arrayC, M, N);
-	arrayD = init2DArray(arrayD, M, N);
-	arrayI = init2DArray(arrayI, M, N);
+	C = init_2d_array(C, M, N);
+	D = init_2d_array(D, M, N);
+	I = init_2d_array(I, M, N);
 
 	// Begin Algorithm
 
-	arrayC[0][0] = 0;
-	//arrayC[0][1] = G;
-	//arrayC[1][0] = G;
-	/*
-	for(i=1; i < M; i++)
-		arrayC[i][0] = arrayC[i-1][0] + H;
-	for(j=1; j < N; j++)
-		arrayC[0][j] = arrayC[0][j-1] + H;
-	*/
-	t = G;
+	C[0][0] = 0;
 
 	for(j=1; j < N; j++)
 	{
 		t = t + H;
-		arrayC[0][j] = t;
-		arrayD[0][j] = t + G;
+		C[0][j] = t;
+		D[0][j] = t + G;
 	}
 
 	t = G;
@@ -96,39 +85,39 @@ double gotohAlgorithm(char *seqA, char *seqB)
 	for(i=1; i < M; i++)
 	{
 		t = t + H;
-		arrayC[i][0] = t;
-		arrayI[i][0] = t + G;
+		C[i][0] = t;
+		I[i][0] = t + G;
 
 		for(j=1; j < N; j++)
 		{
-			arrayI[i][j] = min2(arrayI[i][j-1], arrayC[i][j-1] + G) + H;
-			arrayD[i][j] = min2(arrayD[i-1][j], arrayC[i-1][j] + G) + H;
+			I[i][j] = min2(I[i][j-1], C[i][j-1] + G) + H;
+			D[i][j] = min2(D[i-1][j], C[i-1][j] + G) + H;
 
-			arrayC[i][j] = min3(arrayD[i][j], arrayI[i][j], 
-								arrayC[i-1][j-1] + costTable(seqA[i-1], seqB[j-1], costs));
+			C[i][j] = min3(D[i][j], I[i][j], 
+								C[i-1][j-1] + cost_table(sA[i-1], sB[j-1], costs));
 		}
 	}
 
-	printArrays(arrayC, arrayD, arrayI, M, N);
+	print_2d_arrays(C, D, I, M, N);
 	
-	double cost = arrayC[M-1][N-1];
+	double cost = C[M-1][N-1];
 
 	// Free memory for arrays
 	for(i=0; i<M; i++)
 	{
-		free(arrayC[i]);
-		free(arrayD[i]);
-		free(arrayI[i]);
+		free(C[i]);
+		free(D[i]);
+		free(I[i]);
 	}
 
-	free(arrayC);
-	free(arrayD);
-	free(arrayI);
+	free(C);
+	free(D);
+	free(I);
 	
 	return cost;
 }
 
-double **init2DArray(double **array, int M, int N)
+double **init_2d_array(double **array, int M, int N)
 {
 	int i, j;
 	array = malloc(sizeof(double *) * M);
@@ -143,10 +132,12 @@ double **init2DArray(double **array, int M, int N)
 	return array;
 }
 
-void printArray(double **array, int M, int N) 
+void print_2d_array(double **array, int M, int N) 
 {
 	int i, j;
+
 	for(i=0; i<M; i++)
+	{
 		for(j=0; j<N; j++)
 		{
 			if(j == N-1)
@@ -164,11 +155,12 @@ void printArray(double **array, int M, int N)
 					printf("%.2lf\t", array[i][j]);
 			}
 		}
+	}
 }
 
 double min2(double a, double b)
 {
-  return (a < b) ? a : b;
+	return (a < b) ? a : b;
 }
 
 double min3(double a, double b, double c)
@@ -176,16 +168,16 @@ double min3(double a, double b, double c)
 	return min2(min2(a, b), c);
 }
 
-double costTable(char a, char b, double costs[][128])
+double cost_table(char a, char b, double costs[][128])
 {
-  int i, j;
+	int i, j;
 
 	for(i=0; i<128; i++)
-    for(j=0; j<128; j++)
-      costs[i][j] = MISMATCH;
+    	for(j=0; j<128; j++)
+      		costs[i][j] = MISMATCH;
 
-  for(i=0; i<128; i++)
-    costs[i][i] = MATCH;
+  	for(i=0; i<128; i++)
+    	costs[i][i] = MATCH;
 
 	return costs[a][b];
 }
